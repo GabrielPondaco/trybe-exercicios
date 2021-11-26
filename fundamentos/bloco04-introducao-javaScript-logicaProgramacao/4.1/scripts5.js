881484c6288e2893ab123207d50c95fb9d4c693f
Gabriel Pondaco <gabrielpondaco2013@gmail.com>
2021-11-26 16:06:21 -0300

    script5.js switch e case

---
+++ fundamentos/bloco04-introducao-javaScript-logicaProgramacao/4.1/scripts5.js
@@ -0,0 +1,20 @@
+var estadoCandidato = "lalala";
+
+switch (estadoCandidato){
+    case("aprovada"):
+        console.log("Parabéns, você foi aprovada(o)");
+        break;
+    
+    case("lista"):
+        console.log("Você está na lista de espera");
+        break;
+    
+    case("reprovada"):
+        console.log("Você foi reprovada(o)");
+        break;
+    
+    default:
+        console.log("Não se aplica");
+
+
+}
