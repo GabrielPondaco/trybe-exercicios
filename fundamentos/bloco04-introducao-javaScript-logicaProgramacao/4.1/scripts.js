13ddf64dd8335eca66c31fef73d82fe9fe61ffa0
Gabriel Pondaco <gabrielpondaco2013@gmail.com>
2021-11-26 15:13:35 -0300

    scripts.js conteudo de variaveis

---
+++ fundamentos/bloco04-introducao-javaScript-logicaProgramacao/4.1/scripts.js
@@ -0,0 +1,9 @@
+const myName = "Gabriel" ;
+const birthCity = "Ribeirão Preto";
+var birthYear = 1995;
+
+
+console.log( myName, birthCity, birthYear );
+birthYear = 2001;
+console.log(birthYear);
+// birthCity = "Lalalalau"; constante não pode mudar!!!
