3a3ab7571c5976467652e875bebd8f193bb32a6b
Gabriel Pondaco <gabrielpondaco2013@gmail.com>
2021-11-27 13:47:33 -0300

    segue o commit anterior rs

---
+++ fundamentos/bloco04-introducao-javaScript-logicaProgramacao/4.2/script.js
@@ -0,0 +1,24 @@
+/* let menu = ['Home', 'Serviços', 'Portfólio', 'Links'];
+ let menuServices = menu[1];
+
+ console.log(menuServices);  exercicio 1 
+ let menu = ['Home', 'Serviços', 'Portfólio', 'Links'];
+let indexOfPortfolio = menu.indexOf("Portfólio");
+
+console.log(indexOfPortfolio); exercicio 2 
+let menu = ['Home', 'Serviços', 'Portfólio', 'Links'];
+menu.push("Contato");
+console.log(menu); exercicio 3 ^^^^^^^ ARRAYS */
+//----------------------------------------------------------
+
+/*let groceryList = ['Arroz', 'Feijão', 'Alface', 'Melancia'];
+
+for (let index = 0; index < groceryList.length ; index += 1){
+    console.log(groceryList[index]);
+} exercicio 1 for 
+//----------------------------------------------------------
+let names = ['João', 'Maria', 'Antônio', 'Margarida'];
+for (let name of names){
+    console.log(name);
+} exercicio 1 for of */
+
