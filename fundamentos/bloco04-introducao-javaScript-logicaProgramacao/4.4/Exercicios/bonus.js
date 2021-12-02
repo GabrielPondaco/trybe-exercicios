ec2967a62b7c29048946c317d6f4180fcf6dc589
Gabriel Pondaco <gabrielpondaco2013@gmail.com>
2021-12-02 10:22:30 -0300

    acredito que funcionou os numeros romanos

--- fundamentos/bloco04-introducao-javaScript-logicaProgramacao/4.4/Exercicios/bonus.js
+++ fundamentos/bloco04-introducao-javaScript-logicaProgramacao/4.4/Exercicios/bonus.js
@@ -28,35 +28,57 @@ function identificaRomano (numero) {
     case 'M':
       valor = 1000;
       break;
+
+    default:
+      valor = null;
+      break;
     
   }
   return valor;
 }
 
-function calculaRomano (romano) {
+function transformaArray (array) {
+  let arrayNumeros = [];
+  for (let index of array) {
+    if (typeof(identificaRomano) == null){
+      return 
+    }
+    else {
+      arrayNumeros.push(identificaRomano(index));
+    }
+  }
+
+  return arrayNumeros;
+}
+
+function calculaRomano (array) {
   let sum = 0;
-  
-  for (let index)
-  if (romano.length <= 0){
-    return 'numero inválido';
+  let arrayNumeros = [];
+  arrayNumeros = transformaArray(array);
+  if (arrayNumeros.length <= 0){
+    return 'numeros romanos invalidos!';
   }
-  else if (romano.length === 1){
-    sum = identificaRomano(romano[0]);
-  }
-  else if (identificaRomano(romano[0]) < identificaRomano(romano[1])){
-    sum -= identificaRomano(romano[0]);
+  else if (arrayNumeros.length === 1){
+    sum = arrayNumeros[0];
+    return sum;
   }
   else {
-    sum+= identificaRomano(romano[0]);
+    for (let index = 0; index < arrayNumeros.length; index+=1){
+      if (arrayNumeros[index] >= arrayNumeros[index+1] || index === arrayNumeros.length-1){
+        sum = sum + arrayNumeros[index];
+      }
+      if (arrayNumeros[index] < arrayNumeros[index+1]){
+        sum = sum - arrayNumeros[index];
+      }
+    }
+    return sum;
   }
-
-  return sum;
-
-
+  
 
 
 
 }
-let stringNumeros = 'C';
+let stringNumeros = 'XI';
 
 console.log(calculaRomano(stringNumeros));
+//console.log(calculaRomano(stringNumeros));
