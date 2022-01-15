2f33f232bc9f92610cf8dcc74da9eb39b33865de
Gabriel Pondaco <gabrielpondaco2013@gmail.com>
2022-01-15 08:07:40 -0300

    Exercicio 1 de jest

---
+++ fundamentos/bloco07-javaScriptES6-testes-unitarios/7.3/exercicios/sum.test.js
@@ -0,0 +1,26 @@
+// sum.test.js
+const sum = require('./sum');
+
+describe('testa a função sum', () => {
+  // Teste se o retorno de sum(4, 5) é 9
+  it('expect 4 + 5 to equal 9', () => {
+    expect(sum(4, 5)).toEqual(9);
+  });
+  // Teste se o retorno de sum(0, 0) é 0
+  it('expect 0 + 0 to equal 0', () => {
+    expect(sum(0, 0)).toEqual(0);
+  });
+  // Teste se a função sum lança um erro quando os parâmetros são 4 e "5" (string 5)
+  it('expect sum to throw an error message if a parameter is a string', () => {
+    expect(() => sum(4, '5')).toThrowError();
+  });
+
+  // Teste se a mensagem de erro é "parameters must be numbers" quando realizar a chamada sum(4, "5")
+  it('expect sum to throw an error message if a parameter is a string', () => {
+    expect(() => sum(4, '5')).toThrowError('parameters must be numbers');
+  })
+})
+
+
+
+
