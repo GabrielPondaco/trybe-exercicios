31909b32cbe6a27823ace3088600db0459a7d940
Gabriel Pondaco <gabrielpondaco2013@gmail.com>
2022-01-22 08:07:40 -0300

    Review de reduce, e refatoraçao de algumas funçoes dos ex do 8.4

--- fundamentos/bloco08-high-order-functions-javaScript-ES6/8.4/ex6.js
+++ fundamentos/bloco08-high-order-functions-javaScript-ES6/8.4/ex6.js
@@ -2,11 +2,12 @@ const students = ['Pedro Henrique', 'Miguel', 'Maria Clara'];
 const grades = [[9, 8, 10, 7, 5], [10, 9, 9, 10, 8], [10, 7, 10, 8, 9]];
 
 function calcAverage(index) {
-  return (grades[index].reduce((acc, curr) => acc + curr)) / grades[index].length;
+  return (grades[index]
+    .reduce((acc, curr) => acc + curr)) / grades[index].length;
 }
 
 function studentAverage() {
-  const studentsReduced = students.reduce((acc, curr, index) => {
+  return students.reduce((acc, curr, index) => {
     const stuObj = {
       name: curr,
       average: calcAverage(index),
@@ -14,7 +15,6 @@ function studentAverage() {
     acc.push(stuObj);
     return acc;
   }, []);
-  return studentsReduced;
 }
 console.log(studentAverage());
 // const expected = [
