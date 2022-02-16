e9a679e768a4e53e79b80250ed25abc18791013c
Gabriel Pondaco <gabrielpondaco2013@gmail.com>
2022-02-16 17:26:11 -0300

    lalao

---
+++ fundamentos/bloco09-javaScript-testesAssincronos/9.3/script.js
@@ -0,0 +1,5 @@
+jogador1.innerHTML = 'lalao';
+// ul.appendChild(document.createElement('li'))
+// .innerText = "texto do li";
+const li = document.createElement('li');
+ul.appendChild(li).innerHTML = 'lalu';
